6b319b9677ad6f4a99f8591d9c939e6e83212148
José Barrachina <jbarrachina@ausiasmarch.net>
2015-12-11 19:43:46 +0100

    versión preliminar, funcional, pero mejorable

--- index.php
+++ index.php
@@ -8,15 +8,30 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title>Proyecto CRUD</title>
+        <script type="text/JavaScript">
+            function borra_cliente(id) {
+                var answer = confirm('¿Estás seguro que deseas borrar el cliente?');
+                if (answer) {
+                    // si el usuario hace click en ok, 
+                    // se ejecutar borrar.php
+                    window.location = 'borra.php?id=' + id;
+                }
+            }
+        </script> 
     </head>
     <body> 
         <h1>Listado de Clientes</h1>
         <?php
 // incluir la conexión a la base de datos
         include 'conexion.php';
+        $action = isset($_GET['action']) ? $_GET['action'] : "";
 
+// si viene de borra.php
+        if ($action == 'deleted') {
+            echo "<div>El registro cliente ha sido borrado.</div>";
+        }
 // Elegir los datos que deseamos recuperar de la tabla
-        $query = "SELECT nif, nombre, apellido1, apellido2, email, telefono, usuario  "
+        $query = "SELECT id, nif, nombre, apellido1, apellido2, email, telefono, usuario  "
                 . "FROM clientes "
                 . "ORDER BY apellido1, apellido2, nombre";
         if ($stmt = $conexion->prepare($query)) {
@@ -24,7 +39,7 @@ and open the template in the editor.
                 die('Error de ejecución de la consulta. ' . $conexion->error);
             }
 // recoger los datos
-            $stmt->bind_result($nif, $nombre, $apellido1, $apellido2, $email, $telefono, $usuario);
+            $stmt->bind_result($id, $nif, $nombre, $apellido1, $apellido2, $email, $telefono, $usuario);
 
 // enlace a alta de cliente
             echo "<div>";
@@ -54,11 +69,11 @@ and open the template in the editor.
                 echo "<td>$telefono</td>";
                 echo "<td>$usuario</td>";
                 echo "<td>";
-                // we will use this links on next part of this post
-                echo "<a href='edita.php?nif={$nif}'>Edita</a>";
+// Este enlace es para modificar el registro
+                echo "<a href='edita.php?id={$id}'>Edita</a>";
                 echo " / ";
-                // we will use this links on next part of this post
-                echo "<a href='javascript:borra_cliente(\"$nif\")'> Elimina </a>";
+// Este enlace es para borrar el registro y también se explicará más tarde
+                echo "<a href='javascript:borra_cliente(\"$id\")'> Elimina </a>";
                 echo "</td>";
                 echo "</tr>\n";
             }
